a39408458696cb918df784cbcba45310e2aabefd
eunice <atandainioluwa371@gmail.com>
2025-07-25 16:14:18 -0700

    follow up push

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -11,7 +11,7 @@ export function Navigation() {
           {/* Mobile menu */}
           <Sheet>
             <SheetTrigger asChild>
-              <Button variant="ghost" size="gicon" className="md:hidden">
+              <Button variant="ghost" size="icon" className="md:hidden">
                 <Menu className="h-5 w-5" />
               </Button>
             </SheetTrigger>
